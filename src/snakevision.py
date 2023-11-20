90f71cb9f29013f8aa563528f04424c6cff665c4
skchronicles <kuhnsa3@gmail.com>
2023-11-20 18:02:25 -0500

    Adding abstract base class and adding SnakeVision class and methods

--- src/snakevision.py
+++ src/snakevision.py
@@ -2,6 +2,7 @@
 
 # Python standard library
 from __future__ import print_function
+from abc import ABC, abstractmethod
 import argparse, sys, os, textwrap
 
 # 3rd party package from pypi,
@@ -134,6 +135,10 @@ def parsed_arguments(name, description):
                     • Default: "all" "multiqc"  
                     • Example: --skip-rules All Multiqc FastQC
 
+          -d, --debug-dag
+                Increases verbosity to help debug any DAG errors.
+                    • Example: --debug-dag  
+
           -h, --help
                 Shows usage information, help message, and exits.
                     • Example: --help
@@ -190,6 +195,15 @@ def parsed_arguments(name, description):
         default = ['all', 'multiqc'],
         help = argparse.SUPPRESS
     )
+
+    # Debugs DAG, increases verbosity
+    parser.add_argument(
+        '-d', '--debug-dag',
+        action = 'store_true',
+        default = False,
+        required = False,
+        help = argparse.SUPPRESS
+    )
     
     # Add custom help message
     parser.add_argument(
@@ -209,97 +223,173 @@ def parsed_arguments(name, description):
     return parser.parse_args()
 
 
-def snakevision(dag, output, skip = None):
-    """Creates a snakevision figure from a snakemake DAG.
-    Convert this to a class, add methods for parsing,
-    instantiating, and writing the DAG to an SVG.
+class AbstractSnakeVision(ABC):
+    """An abstract base class for defining an common interface for snakevision. 
+    A snakevision class contains methods for parsing rulegraphs, building a DAG, 
+    and writing the resulting dag as a snakevision image.
+
+    Usage example:
+        dag = SnakeVision('snakemake_rulegraph')
+        dag.write('/path/to/output/pipeline_dag.svg')
     """
-    node2label = {}
-    labels = []
-    n2n = []
-    # Parse snakemake dag
-    # TODO: convert to method
-    for line in dag:
-        line = line.strip()
-        if not line:
-            # skip over empty lines
-            continue
-        if not line[0].isdigit():
-            continue
-        # nodes and edges in dag
-        # parse these to map node
-        # id to snakemake rule
-        # 0[label = "all", color = "0.34 0.6 0.85", style="rounded"];
-        # 1[label = "fc_lane", color = "0.35 0.6 0.85", style="rounded"];
-        # 2[label = "fastqc_raw", color = "0.09 0.6 0.85", style="rounded"];
-        # 1 -> 0
-        # 2 -> 0
-        if '[' in line:
-            # node definition line, i.e:
-            # 0[label = "all", color = "0.34 0.6 0.85", style="rounded"];
-            node = int(line.split('[')[0])
-            label = line.split('=')[1].split(',')[0].strip().replace('"', '').replace("'", "")
-            if label == skip:
-                # Get node ID of skip node, i.e
-                # rule all or the final target
-                skipnode = node
-            node2label[node] = label
-            labels.append(label)
-
-        elif '->' in line:
-            # node relationships or edges, i.e:
-            # 1 -> 0
-            nodes = [int(n.strip()) for n in line.split('->')]
-            lab1 = node2label[nodes[0]]
-            lab2 = node2label[nodes[1]]
-            n2n.append([lab1, lab2])
+    @abstractmethod
+    def parse(self):
+        ...
     
-    # Instantiate the dag, add nodes
-    # TODO: convert to method
-    G = nx.DiGraph()
-    for lab in labels:
-        if lab in skip:
-            # Do NOT include rule all
-            # in the final figure 
-            continue
-        G.add_node(lab)
-        print(lab)
-
-    # Add relationships or edges to dag
-    for l1, l2 in n2n:
-        if l1 in skip or l2 in skip:
-            # Do NOT include rule all
-            # in the final figure 
-            continue
-        G.add_edge(l1, l2)
-        print(l1, "->", l2)
+    @abstractmethod
+    def build(self):
+        ...
     
-    # Write snakevision dag to SVG file
-    # TODO: convert to method
-    r = dagviz.render_svg(G)
-    # Write SVG to a file
-    with open(output, "wt") as fs:
-        fs.write(r)
+    @abstractmethod
+    def write(self):
+        ...  
+
+
+class SnakeVision(AbstractSnakeVision):
+    """SnakeVision class contains methods for parsing snakemake rule graphs, 
+    building a DAG, and writing the resulting dag as a snakevision image.
+
+    Usage example:
+        dag = SnakeVision('snakemake_rulegraph')
+        dag.write('/path/to/output/pipeline_dag.svg')
+    """
+    def __init__(self, input, dag = nx.DiGraph(), skip = [], verbose = False):
+        self.input  = input         # Input open file handle to a snakemake rule graph
+        self.dag    = dag           # networkx.DiGraph() object to model relationships 
+        self.skip   = skip          # Do not add these rules in figure
+        self.verbose     = verbose  # Prints parsed nodes and relationships
+        self.node2label  = {}       # Maps node ids to rule names
+        self.labels      = []       # Keeps track of rule name order
+        self.n2n         = []       # Nested list keeps track of node-to-node relationships
+        self.verbose_dag = ""       # Verbose dag debugging information 
+
+        # Parse input snakemake rule graph.
+        self.parse()
+        # Builds dag from the parsed rule graph
+        self.build()
+        # Collect and display dag debugging information
+        self.debug_dag()
+        if self.verbose:
+            print(self.verbose_dag)
+
+    def __repr__(self):
+        return textwrap.dedent(
+            """
+            Class usage example:
+            dag = SnakeVision('snakemake_rulegraph.txt')
+            dag.write('/path/to/output/pipeline_dag.svg')
+            """
+        )
+    
+    def __str__(self):
+        return self.verbose_dag
+
+    def debug_dag(self):
+        """Displays verbose information of the parsed rule graph."""
+        _c = Colors
+        self.verbose_dag = textwrap.dedent(
+            """
+            {3}{4}Node-to-label mappings:{5}
+            • {0}
+
+            {3}{4}Node to node relationship:{5}
+            • {1}
+
+            {3}{4}Order of encountered labels:{5}
+            • {2}
+            """.format(
+                self.node2label, 
+                self.n2n, self.labels, 
+                _c.bold, _c.url, _c.end
+            )
+        )
+
+    def parse(self):
+        """Parses a snakemake rule graph, saves parsed output in node2labels
+        labels, and n2n instance variables. 
+        
+        Example input rule graph:
+           0[label = "all", color = "0.34 0.6 0.85", style="rounded"];
+           1[label = "fc_lane", color = "0.35 0.6 0.85", style="rounded"];
+           2[label = "fastqc_raw", color = "0.09 0.6 0.85", style="rounded"];
+           1 -> 0
+           2 -> 0
+        """
+        for line in self.input:
+            line = line.strip()
+            # Skip over empty and un-important 
+            # lines, rulegraph node ids start
+            # with a number
+            if not line: continue
+            if not line[0].isdigit(): continue
+            # Parse nodes and edges in dag,
+            # need to map a node id to a 
+            # snakemake rule name
+            if '[' in line:
+                # node definition line, i.e:
+                # 0[label = "all", color = "0.34 0.6 0.85", style="rounded"];
+                node = int(line.split('[')[0])
+                label = line.split('=')[1].split(',')[0].strip().replace('"', '').replace("'", "")
+                if label == self.skip:
+                    # Get node ID of skip node, i.e
+                    # rule all or the final target
+                    skipnode = node
+                self.node2label[node] = label
+                self.labels.append(label)
+            elif '->' in line:
+                # node relationships or edges, i.e:
+                # 1 -> 0
+                nodes = [int(n.strip()) for n in line.split('->')]
+                lab1 = self.node2label[nodes[0]]
+                lab2 = self.node2label[nodes[1]]
+                self.n2n.append([lab1, lab2])
+    
+    def build(self):
+        """Builds a DAG from a parsed snakemake rule graph."""
+        # Add nodes to the dag
+        for lab in self.labels:
+            if lab in self.skip:
+                # Do NOT include rule all
+                # in the final figure 
+                continue
+            self.dag.add_node(lab)
+    
+        # Add relationships or edges to dag
+        for l1, l2 in self.n2n:
+            if l1 in self.skip or l2 in self.skip:
+                # Do NOT include rule all
+                # in the final figure 
+                continue
+            self.dag.add_edge(l1, l2)
+
+    def write(self, output = 'snakevision_dag.svg'):
+        """Write DAG to output image."""
+        # Create output directory as needed
+        outdir = os.path.dirname(os.path.abspath(output))
+        if not os.path.exists(outdir):
+            # Pipeline output directory
+            # does not exist on filesystem
+            os.makedirs(outdir)
+        # Write snakevision dag to SVG file
+        o = dagviz.render_svg(self.dag)
+        # Write SVG to a file
+        with open(output, "wt") as fs:
+            fs.write(o)
 
 
 def main():
     # Parse command-line arguments
     args = parsed_arguments(name = _name, description = _description)
-    
-    # Create output directory as needed
-    outdir = os.path.dirname(os.path.abspath(args.output))
-    if not os.path.exists(outdir):
-        # Pipeline output directory
-        # does not exist on filesystem
-        os.makedirs(outdir)
 
-    # Create a snaekviz DAG
-    snakevision(
-        dag = args.input_file,
-        output = args.output,
-        skip = args.skip_rules
+    # Create a snakevision dag object
+    dag = SnakeVision(
+        input = args.input_file,
+        skip = args.skip_rules,
+        verbose = args.debug_dag
     )
 
+    # Write dag object to file
+    dag.write(output = args.output)
 
 if __name__ == '__main__':
     main()
